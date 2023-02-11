1f2b7e98d4ada53a93a8b4e0087b4ada717c88c6
Moirai7 <moirai.zhang@gmail.com>
2023-02-11 17:33:23 +0000

    SPI race configu

--- SymDignose/Scripts/run.sh
+++ SymDignose/Scripts/run.sh
@@ -2,6 +2,9 @@ python3 SEmu-helper.py RaceTest/STM32G474_LL_USART_Race.elf RaceTest/STM32G474_u
 ./launch-SEmu.sh debug
 mv s2e-out-0/ Statistics/unit-tests/save-k64-uart/
 
+python3 SEmu-helper.py RaceTest/STM32F429I-HAL-SPI-EFlags.elf RaceTest/STM32G474_spi_race.cfg --nlp=RaceTest/f429.txt --signal=RaceTest/spi_signal.txt --cc=RaceTest/spi_cc.txt --debug
+./launch-SEmu.sh debug
+mv s2e-out-0/ Statistics/unit-tests/save-k64-spi/
 
 
 python3 SEmu-helper.py MK64F-unit_tests/K64F-RIOT-USART.elf MK64F-unit_tests/K64F_UART.cfg --nlp=MK64F-unit_tests/k64.txt --signal=MK64F-unit_tests/uart_signal.txt --cc=MK64F-unit_tests/uart_cc.txt --debug
